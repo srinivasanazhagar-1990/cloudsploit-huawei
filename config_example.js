66343f727d21fd29e05aced0f8e7836db28e7a90
Srinivasan Azhagar <74057281+srinivasanazhagar-1990@users.noreply.github.com>
2025-04-19 00:23:37 +0800

    Update config_example.js

--- config_example.js
+++ config_example.js
@@ -2,12 +2,12 @@
 
 module.exports = {
     credentials: {
-        alibaba: {
+	alibaba: {
             // OPTION 1: If using a credential JSON file, enter the path below
             // credential_file: '/path/to/file.json',
             // OPTION 2: If using hard-coded credentials, enter them below
-            // access_key: process.env.ALIBABA_ACCESS_KEY_ID || '',
-            // access_key_secret: process.env.ALIBABA_ACCESS_KEY_SECRET || '',    
+            //   access_key_id: process.env.ALIBABA_ACCESS_KEY_ID || '',
+             //access_key_secret: process.env.ALIBABA_ACCESS_KEY_SECRET || '',
         },
         aws: {
             // OPTION 1: If using a credential JSON file, enter the path below
@@ -59,9 +59,9 @@ module.exports = {
             // OPTION 1: If using a credential JSON file, enter the path below
             // credential_file: process.env.GOOGLE_APPLICATION_CREDENTIALS || '/path/to/file.json',
             // OPTION 2: If using hard-coded credentials, enter them below
-            // project: process.env.GOOGLE_PROJECT_ID || 'my-project',
-            // client_email: process.env.GOOGLE_CLIENT_EMAIL || 'cloudsploit@your-project-name.iam.gserviceaccount.com',
-            // private_key: process.env.GOOGLE_PRIVATE_KEY || '-----BEGIN PRIVATE KEY-----\nYOUR-PRIVATE-KEY-GOES-HERE\n-----END PRIVATE KEY-----\n'
+            // project: process.env.GOOGLE_PROJECT_ID || '',
+            // client_email: process.env.GOOGLE_CLIENT_EMAIL || '',
+            // private_key: process.env.GOOGLE_PRIVATE_KEY || ''
         },
         oracle: {
             // OPTION 1: If using a credential JSON file, enter the path below
@@ -81,6 +81,34 @@ module.exports = {
             // url: process.env.GITHUB_URL || 'https://api.github.com',
             // login: process.env.GITHUB_LOGIN || 'myusername',
             // organization: process.env.GITHUB_ORG || false
-        }
+        },
+	huawei: {
+        	access_key: process.env.HUAWEI_ACCESS_KEY || '',
+        	secret_access_key: process.env.HUAWEI_SECRET_KEY || '',
+		regions: {
+            	    'cn-north-4': {
+                        projectId: '',
+                        endpoint: 'https://vpc.cn-north-4.myhuaweicloud.com'
+                    },
+                    'ap-southeast-1': {
+                        projectId: '',
+                        endpoint: 'https://vpc.ap-southeast-1.myhuaweicloud.com'
+                    },
+                    'ap-southeast-3': {
+                        projectId: '',
+                        endpoint: 'https://vpc.ap-southeast-3.myhuaweicloud.com',
+			region: 'ap-southeast-3'
+                    },
+                    'ap-southeast-2': {
+                        projectId: '',
+                        endpoint: 'https://vpc.ap-southeast-2.myhuaweicloud.com'
+                    },
+                    'cn-east-3': {
+                        projectId: '',
+                        endpoint: 'https://vpc.cn-east-3.myhuaweicloud.com'
+                    }
+                }
+
+    	}
     }
 };
